9f9a0e6f80975ddb354cdf645743f4708e05d40c
NordiAut <71013884+NordiAut@users.noreply.github.com>
2020-10-01 14:23:39 +0200

    Added Models
    Order / Server / ApiContext(Unfinished)

---
+++ Models/ApiContext.cs
@@ -0,0 +1,14 @@
+// using Microsoft.EntityFrameworkCore;
+
+namespace CoreApi.Models
+{
+    public class ApiContext
+    {
+        // Todo fix entity framework
+        // public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+
+        // public DbSet<Customer> Customers { get; set; }
+        // public DbSet<Order> Orders { get; set; }
+        // public DbSet<Server> Servers { get; set; }
+    }
+}
